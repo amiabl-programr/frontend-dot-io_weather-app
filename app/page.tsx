9d3d5b4c8a800f1d0f0f9cecf84d02d1adb2f7b1
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-09-22 08:41:56 +0100

    chore: add assets

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <div>
-      hello
-    </div>
+   <>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-4xl font-bold">How's the sky looking today?</h1>
+    </main>
+   </>
   );
 }
