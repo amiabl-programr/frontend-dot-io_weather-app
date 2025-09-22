4e8a8774c23265192979f8ead655e99f60a3b191
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-09-22 08:26:30 +0100

    initial commit

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <header className="w-full p-4 bg-blue-500 text-white text-center">
+      <h1 className="text-2xl font-bold">Weather App</h1>
+    </header>
+  );
+}
+
+export default Header;
